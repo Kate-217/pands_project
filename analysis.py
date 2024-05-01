6319728110a56355d6fcb25324576331a6694a67
Kate Lisovenko <kate.spb@gmail.com>
2024-05-01 15:37:57 +0100

    scatterplot matrix started

--- analysis.py
+++ analysis.py
@@ -17,6 +17,7 @@ with open("variable_summary", "w") as f:
 
 with open("variable_summary", "a") as f:
     f.write("The summary contains the statistical information about each of the variables.")
+    f.write("\n")
     
 # creating histograms:
 
@@ -27,21 +28,44 @@ def plot_hist_hue(data, column, hue):
     plt.xlabel(f'{column},mm')
     plt.ylabel('Frequency')
 
-fig, axs = plt.subplots(2, 2, figsize=(12, 12))  
-plt.subplot(2,2,1)
+fig, axs = plt.subplots(4, 1, figsize=(20, 20))  
+plt.subplot(4,1,1)
 plot_hist_hue(data=data, column = 'sep_length', hue = 'class')
 
-plt.subplot(2,2,2)
+plt.subplot(4,1,2)
 plot_hist_hue(data=data, column = 'sep_width', hue = 'class')
 
-plt.subplot(2,2,3)
+plt.subplot(4,1,3)
 plot_hist_hue(data=data, column = 'pet_length', hue = 'class')
 
-plt.subplot(2,2,4)
+plt.subplot(4,1,4)
 plot_hist_hue(data=data, column = 'pet_width', hue = 'class')
 
+
+plt.tight_layout()
 plt.savefig('histograms.png')
 plt.close()
 
+# adding scatterplots:
+#with open('scatterplots.png', 'ab') as f:
+    
+    #plt.figure()
+    #pd.plotting.scatter_matrix(data[data['class'] == "Iris-setosa"], figsize=(7, 7), color = 'brown')
+    #plt.suptitle('Relationships between variables for Iris -setosa')
+    
 
-        
+def scatter_plot(axs, data, color, flower_class ):
+    pd.plotting.scatter_matrix(data, color = color, axs=[])
+    plt.suptitle(f'Relationships between variables for {flower_class}')
+
+fig, axs = plt.subplots(2, 1, figsize=(20,20)) 
+plt.subplot(2,1,1)
+scatter_plot(data[data['class']=='Iris-setosa'], color='green', flower_class='Iris-setosa', axs=[0])
+
+plt.subplot(2,1,2)
+scatter_plot(data[data['class']=='Iris-virginica'], color='red', flower_class='Iris-virginica',axs =[1])
+
+    
+plt.tight_layout()
+plt.savefig('scatterplots.png')
+plt.close()     
